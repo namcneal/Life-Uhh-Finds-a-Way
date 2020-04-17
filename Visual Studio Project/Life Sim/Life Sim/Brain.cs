3711a548a64b10c1cd679d887ee00439d95eaed4
namcneal <namcneal@umich.edu>
2020-04-17 16:00:02 +0100

    Added comments to the Brain class to explain what the functions do.

--- Visual Studio Project/Life Sim/Life Sim/Brain.cs
+++ Visual Studio Project/Life Sim/Life Sim/Brain.cs
@@ -6,15 +6,24 @@ public class Brain {
     // Store input neurons in list 1, hidden neurons in list 2, and outputs in list 3
     public List<List<Neuron>> neurons = new List<List<Neuron>>();
 
+    /*
+    Requires: Nothing
+    Modifies: nothing
+    Returns: A new brain instance is created
+    */
     public Brain()
     {
         neurons.Add(new List<Neuron>());
         neurons.Add(new List<Neuron>());
         neurons.Add(new List<Neuron>());
-
-
     }
 
+    /*
+    Requires: The type of neuron to be create
+    Modifies: The brain's list of neurons, adding the neuron with the defauly 
+              bias to the proper sublist
+    Returns: Nothing
+    */
     public void growNeuron(string type)
     {
         switch (type)
@@ -31,6 +40,11 @@ public class Brain {
         }
     }
 
+    /*
+        Requires: The type of neuron to be create, and its bias
+        Effect:  Adds the neuron with the given bias to the proper sublist
+        Returns: Nothing
+    */
     public void growNeuron(string type, float bias)
     {
         switch (type)
@@ -47,11 +61,28 @@ public class Brain {
         }
     }
 
+    /*
+    Requires: Two neurons to be connected, one initial one final. 
+              Also requires the weight of the connection
+    Effect: Modifies the neuron named "initial" through the funcionality of its class.
+            The "terminal" neuron is "connected" to it.
+    Returns: Nothing
+    */
     public void connectNeurons(Neuron initial, Neuron terminal, float connectionWeight)
     {
         initial.addOutgoingConnection(terminal, connectionWeight);
     }
 
+    /*
+        Requires: Nothing
+        Effect: Causes all the input neurons to fire, triggering all neurons connected
+                to them to also fire when appropriate. This eventually causes all 
+                neurons to fire, making the output neurons run their actions
+                if their threshold is crossed.
+        Modifies: All fired neurons have their current value zeroed out. So the network
+                  will have all neuron's set to zero by the end of the function. 
+        Returns: Nothing
+    */
     public void triggerFiringCascade()
     {
         foreach (Neuron neuron in neurons[0])
